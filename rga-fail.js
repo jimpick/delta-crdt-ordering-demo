1d25be3be4f389d277ce4ccfafde83c68ca9b6d9
Jim Pick <jim@backblend.com>
2019-01-24 00:06:16 -0800

    Add trace back, simplify explanation

--- rga-fail.js
+++ rga-fail.js
@@ -1,108 +1,89 @@
 const CRDTs = require('delta-crdts')
 const RGA = CRDTs('rga')
 const rgaType = CRDTs.type('rga')
-const b58Decode = require('bs58').decode
 
-const crdtId = id => b58Decode(id).slice(-4)
+// Let's create the word 'pear' using two replicas
+// 1. push 'p' to replicaPear
+// 2. push 'e' and 'a' to replicaVowels
+// 3. apply replicaVowels state to replicaPear
+// 3. push 'r' to replicaPear ... spelling 'pear'
 
-const replicaX = RGA('1')
-const replicaY = RGA('2')
+const replicaVowels = RGA('id1') // ID sort order matters
+const replicaPear = RGA('id2')
 
-const deltaA = replicaY.push('a')
+const deltaP = replicaPear.push('p')
 
-console.log('replicaY 1', replicaY.value().join(''))
+console.log('replicaPear initial:', replicaPear.value().join(''))
 
-const deltaE = replicaX.push('E')
-const deltaF = replicaX.push('F')
+const deltaE = replicaVowels.push('e')
+const deltaA = replicaVowels.push('a')
 
-console.log('replicaX 1', replicaX.value().join(''))
+console.log('replicaVowels:', replicaVowels.value().join(''))
 
-replicaY.apply(deltaE)
-replicaY.apply(deltaF)
+replicaPear.apply(replicaVowels.state())
 
-console.log('replicaY 2', replicaY.value().join(''))
+console.log('replicaPear after applying vowels:', replicaPear.value().join(''))
 
-const deltaB = replicaY.push('b')
+const deltaR = replicaPear.push('r')
 
-console.log('replicaY 3', replicaY.value().join(''))
+console.log('replicaPear final:', replicaPear.value().join(''))
+
+console.log('')
+console.log('Apply deltas in original order:')
 
 const replica1 = RGA('replica1')
-replica1.apply(deltaA)
+replica1.apply(deltaP)
 replica1.apply(deltaE)
-replica1.apply(deltaF)
-replica1.apply(deltaB)
-console.log('replica1', replica1.value().join(''))
+replica1.apply(deltaA)
+replica1.apply(deltaR)
+console.log('replica1:', replica1.value().join(''))
+
+console.log('')
+console.log('Apply deltas in modified order:')
 
 const replica2 = RGA('replica2')
 replica2.apply(deltaE)
-replica2.apply(deltaF)
 replica2.apply(deltaA)
-replica2.apply(deltaB)
-console.log('replica2', replica2.value().join(''))
+replica2.apply(deltaP)
+replica2.apply(deltaR)
+console.log('replica2:', replica2.value().join(''))
+
+console.log('')
+console.log('Apply deltas in a different order:')
 
 const replica3 = RGA('replica3')
 replica3.apply(deltaE)
-replica3.apply(deltaF)
-replica3.apply(deltaB)
 replica3.apply(deltaA)
-console.log('replica3', replica3.value().join(''))
+replica3.apply(deltaR)
+replica3.apply(deltaP)
+console.log('replica3:', replica3.value().join(''))
+
+console.log('')
+console.log('Apply deltas in another different order:')
 
 const replica4 = RGA('replica4')
 replica4.apply(deltaE)
+replica4.apply(deltaP)
 replica4.apply(deltaA)
-replica4.apply(deltaF)
-replica4.apply(deltaB)
-console.log('replica4', replica4.value().join(''))
+replica4.apply(deltaR)
+console.log('replica4:', replica4.value().join(''))
+
+console.log('')
+console.log('Batch "p" and "r" (fails):')
 
 const replica5 = RGA('replica5')
 replica5.apply(deltaE)
-replica5.apply(deltaF)
-const batchReplica5AB = rgaType.join(deltaA, deltaB)
-replica5.apply(batchReplica5AB)
-console.log('replica5', replica5.value().join(''))
+replica5.apply(deltaA)
+const batchReplica5PR = rgaType.join(deltaP, deltaR)
+replica5.apply(batchReplica5PR)
+console.log('replica5:', replica5.value().join(''))
+
+console.log('')
+console.log('Batch "p" and "r" reversed (fails):')
 
 const replica6 = RGA('replica6')
 replica6.apply(deltaE)
-replica6.apply(deltaF)
-const batchReplica6AB = rgaType.join(deltaB, deltaA)
-replica6.apply(batchReplica6AB)
-console.log('replica6', replica6.value().join(''))
-
-const replica7 = RGA('replica7')
-const batchReplica7EF = rgaType.join(deltaE, deltaF)
-replica7.apply(batchReplica7EF)
-replica7.apply(deltaA)
-replica7.apply(deltaB)
-console.log('replica7', replica7.value().join(''))
-
-const replica8 = RGA('replica8')
-const batchReplica8EF = rgaType.join(deltaF, deltaE)
-replica8.apply(batchReplica8EF)
-replica8.apply(deltaA)
-replica8.apply(deltaB)
-console.log('replica8', replica8.value().join(''))
-
-
-const replicaA = RGA('3')
-const replicaB = RGA('2')
-const replicaC = RGA('1')
-
-replicaA.push('E')
-replicaA.push('F')
-
-console.log('replicaA 1', replicaA.value().join(''))
-
-replicaB.push('b')
-replicaB.apply(replicaA.state())
-
-console.log('replicaB 1', replicaB.value().join(''))
-
-const deltaC_a = replicaC.push('a')
-console.log('replicaC a', replicaC.value().join(''))
-
-replicaB.apply(deltaC_a)
-console.log('replicaB 2', replicaB.value().join(''))
-
-replicaA.apply(deltaC_a)
-console.log('replicaA 2', replicaA.value().join(''))
-
+replica6.apply(deltaA)
+const batchReplica6RP = rgaType.join(deltaR, deltaP)
+replica6.apply(batchReplica6RP)
+console.log('replica6:', replica6.value().join(''))
